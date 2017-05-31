287b91793dadff7c5e5eb191dfee7bda090a348e
Mister Júnio <ruca.33.maia@gmail.com>
2017-05-31 12:45:52 +0900

    fixed small miss in navigator outside tabbar with Angular2

--- patterns/navigator-outside-tabbar-angular2/src/app/page-nav-2.ts
+++ patterns/navigator-outside-tabbar-angular2/src/app/page-nav-2.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OnsNavigator, Params } from 'angular2-onsenui';
 
 @Component({
   selector: 'ons-page',
   template: require('./page-nav-2.html')
 })
-export class PageNav2Component {
+export class PageNav2Component implements OnInit {
   constructor(private _params: Params) { }
 
   cardTitle: string = 'Custom Card';
